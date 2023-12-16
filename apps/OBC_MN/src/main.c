314af9c0772fa1ccfd94a2823838a975df0f8eab
gael <besnier.gael29@email.com>
2023-12-16 15:31:10 +0100

    okii

--- apps/OBC_MN/src/main.c
+++ apps/OBC_MN/src/main.c
@@ -9,7 +9,7 @@ application.
 
 \ingroup module_demo_mn_console
 *******************************************************************************/
-//
+////
 /*------------------------------------------------------------------------------
 Copyright (c) 2017, B&R Industrial Automation GmbH
 Copyright (c) 2013, SYSTEC electronic GmbH
