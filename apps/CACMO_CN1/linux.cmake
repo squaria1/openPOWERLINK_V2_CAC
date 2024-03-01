43bee8e9b17b9d51266d8555993f1703f74b4996
Mael <mael@gmail.com>
2024-03-01 19:03:19 +0100

    ok

--- apps/CACMO_CN1/linux.cmake
+++ apps/CACMO_CN1/linux.cmake
@@ -69,7 +69,7 @@ ELSE (PCAP_CONFIG)
 ENDIF (PCAP_CONFIG)
 
 ENDIF (CFG_KERNEL_STACK_DIRECTLINK OR CFG_KERNEL_STACK_USERSPACE_DAEMON)
-SET (ARCH_LIBRARIES ${ARCH_LIBRARIES} pthread rt)
+SET (ARCH_LIBRARIES ${ARCH_LIBRARIES} pthread rt gpiod)
 
 ################################################################################
 # Set architecture specific installation files
